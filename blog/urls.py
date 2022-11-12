32dde1f95715efdefcc33aaeee4c5d32de711943
integerJI <jjs9536@gmail.com>
2022-11-12 23:56:22 +0900

    Feat: json data add

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.posts, name='posts'),
-    path('post/<int:id>/', views.post_detail, name='post_detail'), # <<<<<<<<<<<< here
+    path('post/<int:id>/', views.post_detail, name='post_detail'),
+    path('json_posts/', views.json_posts, name='json_posts'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404 # <<<<<<<<<<<< here
+from django.shortcuts import render, get_object_or_404
+from django.core import serializers
+from django.http import HttpResponse
 from .models import Post
 
 
@@ -7,7 +9,11 @@ def posts(request):
         published_at__isnull=False).order_by('-published_at')
     return render(request, 'blog/posts.html', {'posts': posts})
 
-# <<<<<<<<<<<< here
 def post_detail(request, id):
     post = get_object_or_404(Post, id=id)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def json_posts(request):
+    posts = Post.objects.filter(published_at__isnull=False).order_by('-published_at')
+    post_list = serializers.serialize('json', posts)
+    return HttpResponse(post_list, content_type="text/json-comment-filtered")
